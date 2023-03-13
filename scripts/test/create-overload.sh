a7dd9cd3d2911c78ae3774d94f436550e687baf0
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-13 12:01:43 +0300

    create overload test

---
+++ scripts/test/create-overload.sh
@@ -0,0 +1,8 @@
+ABS_CLUSTER_PATH="/home/batuhanc/Documents/pire-store/scripts/cluster"
+
+$ABS_CLUSTER_PATH/send.sh create a 42 &
+sleep 0.025 ; $ABS_CLUSTER_PATH/send.sh create b 84 &
+sleep 0.025 ; $ABS_CLUSTER_PATH/send.sh create c 21 &
+sleep 0.025 ; $ABS_CLUSTER_PATH/send.sh create d 63 &
+sleep 0.025 ; $ABS_CLUSTER_PATH/send.sh create e 105
+
