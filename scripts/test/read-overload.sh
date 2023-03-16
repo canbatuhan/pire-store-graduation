3a44e39fe2725e8735d01764607bb1a0c52aa418
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-16 12:16:32 +0300

    tests fixed

--- scripts/test/read-overload.sh
+++ scripts/test/read-overload.sh
@@ -1,12 +1,12 @@
 ABS_CLUSTER_PATH="/home/batuhanc/Documents/pire-store/scripts/cluster"
 
-$ABS_CLUSTER_PATH/send.sh read a &
-$ABS_CLUSTER_PATH/send.sh read b &
-$ABS_CLUSTER_PATH/send.sh read c &
-$ABS_CLUSTER_PATH/send.sh read a &
-$ABS_CLUSTER_PATH/send.sh read b &
-$ABS_CLUSTER_PATH/send.sh read c &
-$ABS_CLUSTER_PATH/send.sh read a &
-$ABS_CLUSTER_PATH/send.sh read b &
-$ABS_CLUSTER_PATH/send.sh read c
-
+COUNT=0
+N_ITERATIONS=4
+while [ $COUNT -le $N_ITERATIONS ] ; do
+    $ABS_CLUSTER_PATH/send.sh read a &
+    $ABS_CLUSTER_PATH/send.sh read b &
+    $ABS_CLUSTER_PATH/send.sh read c &
+    $ABS_CLUSTER_PATH/send.sh read d &
+    $ABS_CLUSTER_PATH/send.sh read e &
+    COUNT=$(($COUNT+1))
+done
