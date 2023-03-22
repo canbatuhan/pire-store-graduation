9572c4f2842a57050611c3c09ad178a2f9643086
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-22 13:16:06 +0300

    switch and update

--- scripts/util/update-pire.sh
+++ scripts/util/update-pire.sh
@@ -5,24 +5,26 @@ NEWLINE="echo """
 USERNAME="pi_user"
 TEMPLATE="192.168.1.12"
 PASSWORD="tolga.halit.batu"
-
-# Commands
-CD="cd /home/batuhan/pire-store"
-SWITCH="sudo git switch cluster"
-PULL="sudo git pull origin"
-EXIT="exit"
-
-# Script to execute
-SCRIPT="$CD;$SWITCH;$PULL;$EXIT"
+BRANCH="cluster"
 
 # Parse arguments
 while getopts :as:f: flag ; do
     case "${flag}" in
+        b) BRANCH=${OPTARG};;
         a) START=0; FINISH=9;;
         s) START=${OPTARG};;
         f) FINISH=${OPTARG};;
     esac
 done
+
+# Commands
+CD="cd /home/batuhan/pire-store"
+SWITCH="sudo git switch $BRANCH"
+PULL="sudo git pull origin"
+EXIT="exit"
+
+# Script to execute
+SCRIPT="$CD;$SWITCH;$PULL;$EXIT"
 
 # Update pire-store
 while [ $START -le $FINISH ] ; do
