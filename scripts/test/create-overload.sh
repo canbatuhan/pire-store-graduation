53853990c8ca30a618332482b4c5bacc59c12dcc
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-24 13:06:22 +0300

    8-paired creaate test

--- scripts/test/create-overload.sh
+++ scripts/test/create-overload.sh
@@ -1,8 +1,11 @@
 ABS_CLUSTER_PATH="/home/batuhanc/Documents/pire-store/scripts/cluster"
 
 $ABS_CLUSTER_PATH/send.sh create a 42 &
-sleep 0.05 ; $ABS_CLUSTER_PATH/send.sh create b 84 &
-sleep 0.05 ; $ABS_CLUSTER_PATH/send.sh create c 21 &
-sleep 0.05 ; $ABS_CLUSTER_PATH/send.sh create d 63 &
-sleep 0.05 ; $ABS_CLUSTER_PATH/send.sh create e 105
+$ABS_CLUSTER_PATH/send.sh create b 84 &
+$ABS_CLUSTER_PATH/send.sh create c 21 &
+$ABS_CLUSTER_PATH/send.sh create d 63 &
+$ABS_CLUSTER_PATH/send.sh create e 105 &
+$ABS_CLUSTER_PATH/send.sh create f 1773 &
+$ABS_CLUSTER_PATH/send.sh create g 1984 &
+$ABS_CLUSTER_PATH/send.sh create h 2501 &
 
