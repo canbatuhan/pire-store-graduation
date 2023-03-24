06c3e51c25f6971b66ca9f0da95fa8982125b351
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-24 13:04:59 +0300

    8-paired create test

--- scripts/test/create-overload.sh
+++ scripts/test/create-overload.sh
@@ -4,5 +4,8 @@ $ABS_CLUSTER_PATH/send.sh create a 42 &
 $ABS_CLUSTER_PATH/send.sh create b 84 &
 $ABS_CLUSTER_PATH/send.sh create c 21 &
 $ABS_CLUSTER_PATH/send.sh create d 63 &
-$ABS_CLUSTER_PATH/send.sh create e 105
+$ABS_CLUSTER_PATH/send.sh create e 105 &
+$ABS_CLUSTER_PATH/send.sh create f 1773 &
+$ABS_CLUSTER_PATH/send.sh create g 2501 &
+$ABS_CLUSTER_PATH/send.sh create h 1984
 
