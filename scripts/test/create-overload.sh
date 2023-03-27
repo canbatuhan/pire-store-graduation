cf1e58504af42573d1988f37482c734aba64b464
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-03-27 14:21:23 +0300

    harder tests

--- scripts/test/create-overload.sh
+++ scripts/test/create-overload.sh
@@ -8,4 +8,21 @@ $ABS_CLUSTER_PATH/send.sh create e 105 &
 $ABS_CLUSTER_PATH/send.sh create f 1773 &
 $ABS_CLUSTER_PATH/send.sh create g 2501 &
 $ABS_CLUSTER_PATH/send.sh create h 1984 &
-
+$ABS_CLUSTER_PATH/send.sh create i 6452 &
+$ABS_CLUSTER_PATH/send.sh create j 6842 &
+$ABS_CLUSTER_PATH/send.sh create k 2416 &
+$ABS_CLUSTER_PATH/send.sh create l 663 &
+$ABS_CLUSTER_PATH/send.sh create m 6542 &
+$ABS_CLUSTER_PATH/send.sh create n 143 &
+$ABS_CLUSTER_PATH/send.sh create o 2201 &
+$ABS_CLUSTER_PATH/send.sh create p 14 &
+$ABS_CLUSTER_PATH/send.sh create r 671 &
+$ABS_CLUSTER_PATH/send.sh create s 54 &
+$ABS_CLUSTER_PATH/send.sh create t 365 &
+$ABS_CLUSTER_PATH/send.sh create u 6176 &
+$ABS_CLUSTER_PATH/send.sh create v 43 &
+$ABS_CLUSTER_PATH/send.sh create w 612 &
+$ABS_CLUSTER_PATH/send.sh create x 54 &
+$ABS_CLUSTER_PATH/send.sh create y 617 &
+$ABS_CLUSTER_PATH/send.sh create z 768 &
+sleep 1
