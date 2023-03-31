edd2cb026bca5c8bd0d1a45a7ad48a2dd90bc3a5
Batuhan Can <batuhanosmancan@gmail.com>
2023-03-31 17:12:49 +0300

    installing etcd to compare

--- scripts/setup/setup-env.sh
+++ scripts/setup/setup-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./install-python.sh -a
-./install-modules.sh -a
-./clone-pire.sh -a
-./set-files.sh -a
+./install-python.sh -s 5 -f 9
+./install-modules.sh -s 5 -f 9
+./clone-pire.sh -s 5 -f 9
+./set-files.sh -s 5 -f 9
