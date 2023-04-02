eed6ad271bed877668fff1c19bd0de5f82bdc351
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-04-02 19:04:22 +0300

    copying from sample state machine object

--- pire/client.py
+++ pire/client.py
@@ -28,12 +28,13 @@ class PireClient(pirestore_pb2_grpc.PireKeyValueStoreServicer):
         self.__comm_handler = CommunicationHandler(self.__id, config_paths.get("topology"))
         self.__pair_machine_map:Dict[bytes,ReplicatedStateMachine] = dict()
         self.__statemachine_config = config_paths.get("statemachine")
+        self.__sample_statemachine = ReplicatedStateMachine(self.__statemachine_config)
         self.__database = LocalDatabase()
         self.__history:List[int] = list()
 
     def __create_statemachine_if_not_exists(self, key:bytes) -> None:
         if self.__pair_machine_map.get(key) == None:
-            statemachine = ReplicatedStateMachine(self.__statemachine_config)
+            statemachine = self.__sample_statemachine
             statemachine.start()
             self.__pair_machine_map.update({key: statemachine})
     
