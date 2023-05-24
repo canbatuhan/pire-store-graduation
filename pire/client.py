97d445a332678a40bdc3a3adae7c00da62a7ced6
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-05-25 01:06:46 +0300

    refix

--- pire/client.py
+++ pire/client.py
@@ -307,13 +307,15 @@ class PireClient(pirestore_pb2_grpc.PireKeyValueStoreServicer):
 
             # Send acknowledgement to user
             user_handler.send_ack(connection, addr, ack, read_value)
+            user_handler.close_connection(connection, addr)
             self.__pair_machine_map.get(key).trigger(Events.DONE)
             
         except PollingTimeoutException: # Try to receive/close
             user_handler.send_ack(connection, addr, False, 0)
+            user_handler.close_connection(connection, addr)
 
         except InvalidRequestType: # Try to receive/close
-            pass
+            user_handler.close_connection(connection, addr)
 
         except ConnectionLostException:
             pass
@@ -326,7 +328,6 @@ class PireClient(pirestore_pb2_grpc.PireKeyValueStoreServicer):
         while True: # Infinite loop
             connection, addr = user_handler.establish_connection()
             request_handler_pool.submit(self.__request_handler_callback, connection, addr)
-            user_handler.close_connection(connection, addr)
             
     """ Helper Functions End """
 
