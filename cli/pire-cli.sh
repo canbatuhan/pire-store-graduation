549beca161b322fab5a2d65269d8f925dfb03e07
ituitis-canb18 <canbatuhan.official@gmail.com>
2023-09-07 11:54:46 +0300

    cli fix

--- cli/pire-cli.sh
+++ cli/pire-cli.sh
@@ -42,7 +42,6 @@ send_request() {
     local method=$3
     local key=$4
     local value=$5
-    echo "send to http://$address$path"
     curl -X "$method" "http://$address$path" -d "key=$key&value=$value"
 }
 
